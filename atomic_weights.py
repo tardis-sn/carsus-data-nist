f9becbfd0ce8480900b3dba7476c85214c6b406d
Shikha Rathi <65958533+s-rathi@users.noreply.github.com>
2023-08-03 20:16:35 +0530

    Add docstrings

--- atomic_weights.py
+++ atomic_weights.py
@@ -35,6 +35,21 @@ def download_weightscomp(ascii='ascii2', isotype='some'):
 
 # Check and create a path to save files
 def check_folders(folder_name, file_name):
+    """
+    Check if the folder exists, and create it if it doesn't.
+
+    Parameters
+    ----------
+    folder_name : str
+        The folder name to check or create.
+    file_name : str
+        The name of the file.
+
+    Returns
+    -------
+    str
+        The full file path.
+    """
     if not os.path.exists(folder_name):  # to check if the folder exists, and create it if not
         os.makedirs(folder_name)
 
@@ -43,6 +58,18 @@ def check_folders(folder_name, file_name):
     
 # Format the data
 def parse_html_content(html_content):
+    """
+    Parses the HTML content and saves the data as CSV.
+
+    Parameters
+    ----------
+    html_content : str
+        The preformatted HTML content.
+
+    Returns
+    -------
+    None
+    """
     html_file_path = check_folders('html_files', 'weights.html')
     with open(html_file_path, "w", encoding="utf-8") as file:   # Save the html data to a file
         file.write(html_content)
@@ -67,4 +94,5 @@ def parse_html_content(html_content):
     df.to_csv(csv_file_path, index=False)
     return
         
-atomic_weights = parse_html_content(download_weightscomp())
+if __name__ == "__main__":
+    atomic_weights = parse_html_content(download_weightscomp())
