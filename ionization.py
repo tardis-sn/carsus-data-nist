b2452262c202e990766f2c952f359138a859e58f
Shikha Rathi <65958533+s-rathi@users.noreply.github.com>
2023-08-08 06:26:29 +0530

    Remove download ionization energies function

--- ionization.py
+++ ionization.py
@@ -2,48 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
-
-IONIZATION_ENERGIES_URL = 'https://physics.nist.gov/cgi-bin/ASD/ie.pl'
-
-def download_ionization_energies(
-        spectra='h-uuh',
-        e_out=0,
-        e_unit=1,
-        format_=1,
-        at_num_out=True,
-        sp_name_out=False,
-        ion_charge_out=True,
-        el_name_out=True,
-        seq_out=False,
-        shells_out=True,
-        conf_out=False,
-        level_out=True,
-        ion_conf_out=False,
-        unc_out=True,
-        biblio=False):
-    """
-        Downloader function for the Ionization Energies Data from the NIST Atomic Spectra Database
-        Parameters
-        ----------
-        spectra: str
-            (default value = 'h-uuh')
-        Returns
-        -------
-        str
-            Preformatted text data
-        """
-    data = {'spectra': spectra, 'units': e_unit,
-            'format': format_, 'at_num_out': at_num_out, 'sp_name_out': sp_name_out,
-            'ion_charge_out': ion_charge_out, 'el_name_out': el_name_out,
-            'seq_out': seq_out, 'shells_out': shells_out, 'conf_out': conf_out,
-            'level_out': level_out, 'ion_conf_out': ion_conf_out, 'e_out': e_out,
-            'unc_out': unc_out, 'biblio': biblio}
-
-    data = {k: v for k, v in data.items() if v is not False}
-    data = {k:"on" if v is True else v for k, v in data.items()}
-   
-    r = requests.post(url=IONIZATION_ENERGIES_URL, data=data)
-    return r.text
+from carsus.io.nist.ionization import download_ionization_energies
 
 def check_folders(folder_name, file_name):
     """
@@ -87,7 +46,7 @@ def parse_html_content(html_data):
     
     table_data = [row for row in table_data if len(row) > 1] # Remove empty rows
 
-    column = ['At. Num', 'Ion Charge', 'El. Name', 'Ground Shells', 'Ground Level', 'Ionization Energy (eV)', 'Uncertainty (eV)', 'x']
+    column = ['At. Num', 'Ion Charge', 'Ground Shells', 'Ground Level', 'Ionization Energy (eV)', 'Uncertainty (eV)', 'x']
     df = pd.DataFrame(table_data[2:], columns=column)
     df = df.drop(df.columns[-1], axis=1)
 
