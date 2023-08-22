d06cf5d3e2654cc5b541468b962487ea6c6e095a
Shikha Rathi <65958533+s-rathi@users.noreply.github.com>
2023-08-22 21:09:12 +0530

    Add NIST data (#1)
    * Create ionization.py
    
    * Create requirements.txt
    
    * Create ionization_energies.yml
    
    * Change NIST to nist
    
    * Add id to steps
    
    * Add csv file format
    
    * Add import pandas
    
    * Add pandas
    
    * Change file name
    
    * Change job ID
    
    * Save html content
    
    * Add save html data
    
    * Remove html5lib installation
    
    * Add docstrings
    
    * Add python script for atomic weights
    
    * Create atomic_weights.py
    
    * Update and rename ionization_energies.yml to nist_data.yml
    
    * Clone carsus
    
    * Modify install carsus
    
    * Remove download weights function
    
    * Remove download ionization energies function
    
    * Add job id
    
    * Change job Iid
    
    * Remove requirements.txt
    
    * Replace nist ionization by nist data
    
    * Force remove carsus
    
    * sort python files
    
    * Remove check_folder func
    
    * Update ionization.py
    
    * Create common_utils.py
    
    * Add import check_folders from common_utils.py
    
    * Import check_folders
    
    * Add python path
    
    * rename common_utils

---
+++ atomic_weights.py
@@ -0,0 +1,47 @@
+import os
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from common_utils import check_folders
+from carsus.io.nist.weightscomp import download_weightscomp
+
+
+# Format the data
+def parse_weights_html_content(html_content):
+    """
+    Parses the HTML content and saves the data as CSV.
+    Parameters
+    ----------
+    html_content : str
+        The preformatted HTML content.
+    Returns
+    -------
+    None
+    """
+    html_file_path = check_folders('html_files', 'weights.html')
+    with open(html_file_path, "w", encoding="utf-8") as file:   # Save the html data to a file
+        file.write(html_content)
+
+    data = []
+    lines = html_content.strip().split('\n')
+    entry = {}
+
+    for line in lines:
+        if '=' in line:
+            key, value = line.split('=', 1)
+            entry[key.strip()] = value.strip()
+        else:
+            data.append(entry)
+            entry = {}
+
+    data.append(entry)  # Append the last entry    
+    df = pd.DataFrame(data)
+    df = df.iloc[2:]
+
+    csv_file_path = check_folders('nist_data', 'weights.csv')
+    df.to_csv(csv_file_path, index=False)
+    return
+
+if __name__ == "__main__":
+    atomic_weights = parse_weights_html_content(download_weightscomp())
---
+++ ionization.py
@@ -0,0 +1,44 @@
+import os
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from common_utils import check_folders
+from carsus.io.nist.ionization import download_ionization_energies
+
+        
+def parse_ionization_html_content(html_data):
+    """
+    Parses the HTML content to extract ionization energy data and saves it to a CSV file.
+    
+    Parameters:
+        html_data (str): The HTML data to parse.
+    """
+    html_file_path = check_folders('html_files', 'ionization_energies.html')
+    with open(html_file_path, "w", encoding="utf-8") as file:  # Save the html data to a file
+        file.write(html_data)
+    
+    soup = BeautifulSoup(html_data, 'html5lib')
+    pre_element = soup.find('pre')
+    pre_text_data = pre_element.get_text()
+    
+    rows = pre_text_data.strip().split('\n')
+
+    table_data = []
+    for row in rows:
+        cells = row.split('|')
+        cleaned_cells = [cell.strip() for cell in cells]
+        table_data.append(cleaned_cells)
+    
+    table_data = [row for row in table_data if len(row) > 1] # Remove empty rows
+
+    column = ['At. Num', 'Ion Charge', 'Ground Shells', 'Ground Level', 'Ionization Energy (eV)', 'Uncertainty (eV)', 'x']
+    df = pd.DataFrame(table_data[2:], columns=column)
+    df = df.drop(df.columns[-1], axis=1)
+
+    csv_file_path = check_folders('nist_data', 'ionization_energies.csv')
+    df.to_csv(csv_file_path, index=False)
+    return
+
+if __name__ == "__main__":
+        ionization_energies = parse_ionization_html_content(download_ionization_energies())
